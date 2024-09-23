b10a13e232b4015263877de6b90afb4dabceee67
OLDGROT <sashawhite10596@gmail.com>
2024-09-23 22:17:31 +0300

    Front
    Table Products

--- public/js/tablescript.js
+++ public/js/tablescript.js
@@ -1,12 +1,13 @@
+const queue = [1,2,12,3,23,23,2,3,213,2,3,23,2,13,23,23];
 const queueElement = document.getElementById('queue');
 const addButton = document.getElementById('addButton');
 
 addButton.addEventListener('click', () => {
-    queueElement.innerHTML = ''; // Очищаем текущий вывод
     queue.forEach(item => {
         const div = document.createElement('div');
         div.className = 'item';
-        div.textContent = item;
+        div.textContent = 'sadasd';
         queueElement.appendChild(div);
+        console.log('addButton clicked');
     });
 });
