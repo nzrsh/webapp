2e4b04552fd9a34d0c24a47e56d29b3e6bda3467
nzrsh <154097651+nzrsh@users.noreply.github.com>
2024-09-27 19:46:34 +0300

    front chapter 1

--- public/js/regscript.js
+++ public/js/regscript.js
@@ -1,50 +1,7 @@
-const login = document.getElementById('login');
-const password = document.getElementById('password');
-const repeatPassword = document.getElementById('repeatPassword');
-const error = document.getElementById('error');
+const login = document.getElementById("login");
+const password = document.getElementById("password");
+const repeatPassword = document.getElementById("repeatPassword");
 
-repeatPassword.addEventListener('input', function() {
-    if (password.value !== repeatPassword.value) {
-        error.style.display = 'block';
-        error.textContent = 'Пароли не совпадают';
-    } else {
-        error.style.display = 'none';
-    }
-});
-
-document.getElementById('reg-button').addEventListener('click', async () => {
-    // Проверка, что пароли совпадают перед отправкой формы
-    if (password.value !== repeatPassword.value) {
-        error.style.display = 'block';
-        error.textContent = 'Пароли не совпадают';
-        return; // Прекращаем выполнение, если пароли не совпадают
-    }
-
-    const newProduct = {
-        login: login.value,
-        password: password.value
-    };
-
-    try {
-        const response = await fetch('/auth/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newProduct)
-        });
-
-        if (!response.ok) {
-            throw new Error('Ошибка сети: ' + response.status);
-        }
-
-        const result = await response.json();
-        console.log(result);
-        console.log('Пользователь создан:', result);
-
-        alert('Пользователь успешно зарегистрирован!');
-        window.location.href = '/login';
-    } catch (error) {
-        console.error("Ошибка с сервера:", error.message);
-    }
-});
+function register() {
+    console.log("Кнопка нажата.")
+}
