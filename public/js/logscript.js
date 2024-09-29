2c92d83c0158f06c468d89d67e0a1adb39d8f518
OLDGROT <sashawhite10596@gmail.com>
2024-09-29 19:59:16 +0300

    admins

--- public/js/logscript.js
+++ public/js/logscript.js
@@ -1,3 +1,39 @@
 function goToReg(){
     document.location.href = "/register";;
 }
+
+async function login(event) {
+    event.preventDefault();
+    console.log("+");
+
+    const login = document.getElementById("login").value;
+    const password = document.getElementById("password").value;
+
+
+    const creds = {
+        login: login,
+        password: password
+    };
+    
+    console.log("Данные: ", creds);
+
+    // Исправлено название переменной
+    const response = await fetch("/auth/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(creds) // Исправлено формирование JSON
+    });
+
+    // Исправлено: используем 'response' вместо 'responce'
+    if (response.status === 201) {
+        window.location.href = "/"
+    } else if (response.status === 401) {
+        const message = await response.text();
+        alert(message); 
+    } else {
+        alert('Ошибка Авторизации. Попробуйте позже.');
+    }
+
+}
