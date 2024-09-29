e06420046bc7c3d79dfcd208c2e293d2d78afe1d
nzrsh <154097651+nzrsh@users.noreply.github.com>
2024-09-30 00:49:27 +0300

    test

--- cmd/routes.go
+++ cmd/routes.go
@@ -16,6 +16,8 @@ func SetupRouter() *httprouter.Router {
 	r.GET("/login", loginPageHandler)
 	r.GET("/register", registerPageHandler)
 	r.GET("/storage", JWTAuthMiddleware(storagePageHandler))
+	r.GET("/edit", JWTAuthMiddleware(editPageHandler))
+	r.GET("/add", JWTAuthMiddleware(addPageHandler))
 
 	//AUTH HANDLERS
 
