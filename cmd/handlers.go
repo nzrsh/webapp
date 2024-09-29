5d1b670071237fa4ad74c8cfa9a83307d2c89c58
nzrsh <154097651+nzrsh@users.noreply.github.com>
2024-09-29 19:36:26 +0300

    storage stage 1

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
-	"strconv"
 
 	_ "github.com/dgrijalva/jwt-go"
 	"github.com/julienschmidt/httprouter"
@@ -25,107 +21,6 @@ func loginPageHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Param
 	http.ServeFile(w, r, "./public/html/login.html")
 }
 
-// API
-// Отдаёт пользователю JSON массив объектов Product, исключая изображение
-func getProductsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	products, err := GetProductsFromTable()
-	if err != nil {
-		log.Printf("getProductsHandler | Ошибка получения списка продуктов: %s", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	err = json.NewEncoder(w).Encode(products)
-	if err != nil {
-		log.Printf("getProductsHandler | Ошибка при кодировании списка продуктов: %s\n", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-// Отдаёт пользователю JSON объект продукта по его ID
-func getProductHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	idStr := ps.ByName("id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		log.Printf("getProductHandler | Ошибка при парсинге айди: %s\n", err)
-		http.Error(w, "Некорректный ID", http.StatusBadRequest)
-		return
-	}
-
-	product, err := GetProductFromTable(id)
-	if err != nil {
-		log.Printf("getProductHandler | Ошибка при получении продукта: %s\n", err)
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(product)
-
-}
-
-func updateProductHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	idStr := ps.ByName("id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		log.Printf("updateProductHandler | Ошибка при получении ID продукта: %s\n", err)
-		http.Error(w, "Некорректный ID", http.StatusBadRequest)
-		return
-	}
-
-	var product Product
-	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
-		log.Printf("updateProductHandler | Ошибка при десереализации продукта: %s\n", err)
-		http.Error(w, "Некорректный запрос", http.StatusBadRequest)
-		return
-	}
-
-	// Обновляем продукт в базе данных
-	err = UpdateProductFromTable(id, product)
-	if err != nil {
-		log.Printf("updateProductHandler | Ошибка при обновлении продукта: %s\n", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Продукт с ID %d успешно обновлен.", id)
-}
-
-func deleteProductHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	idStr := ps.ByName("id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		log.Printf("deleteProductHandler | Ошибка при получении ID продукта: %s\n", err)
-		http.Error(w, "Некорректный ID", http.StatusBadRequest)
-		return
-	}
-	err = DeleteProductFromTable(id)
-	if err != nil {
-		log.Printf("deleteProductHandler | Ошибка удаления продукта: %s\n", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Продукт с ID %d успешно удален.", id)
-}
-
-func createProductHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var product Product
-	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
-		log.Printf("createProductHandler | Ошибка при получении ID продукта: %s\n", err)
-		http.Error(w, "Некорректный запрос", http.StatusBadRequest)
-		return
-	}
-
-	id, err := CreateProductInTable(product)
-	if err != nil {
-		log.Printf("createProductHandler | Ошибка создании продукта: %s\n", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "Продукт %d %s успешно создан.", id, product.Name)
+func storagePageHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.ServeFile(w, r, "./public/html/storage.html")
 }
--- cmd/routes.go
+++ cmd/routes.go
@@ -15,17 +15,18 @@ func SetupRouter() *httprouter.Router {
 	r.GET("/", homePageHandler)
 	r.GET("/login", loginPageHandler)
 	r.GET("/register", registerPageHandler)
+	r.GET("/storage", JWTAuthMiddleware(storagePageHandler))
 
 	//AUTH HANDLERS
 
 	r.POST("/auth/register", registerHandler)
 	r.POST("/auth/login", loginHandler)
 	r.GET("/auth/logout", logoutHandler)
+
 	//Выдача данных о пользователе по токену
 	r.GET("/auth/me", meHandler)
 
-	//API HANDLERS
-
+	//API PRODUCT HANDLERS
 	//Эндпоинт получения списка продуктов в формате массива объектов json
 	r.GET("/api/products", getProductsHandler)
 	//Эндпоинт получения продукта в формате объекта json
@@ -37,5 +38,9 @@ func SetupRouter() *httprouter.Router {
 	//Эндпоинт добавления продукта
 	r.POST("/api/products", JWTAuthMiddleware(createProductHandler))
 
+	//API STORAGE HANDLERS
+	r.GET("/storage/files", JWTAuthMiddleware(getFilesHandler))
+	r.POST("/storage/files/upload", JWTAuthMiddleware(uploadFileHandler))
+	r.GET("/storage/files/image", JWTAuthMiddleware(getImageHandler))
 	return r
 }
